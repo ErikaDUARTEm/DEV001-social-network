60888e3918d7622fe437088345f0811b8f11a5a7
Gabriela Wongchuig <gaba@MacBook-Air-de-Gabriela.local>
2022-11-25 11:06:11 -0500

    Se hizo función de restablecer contraseña con email

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,11 +1,12 @@
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup } from 'firebase/auth';
-import { GoogleAuthProvider } from 'firebase/auth';
+import { GoogleAuthProvider, sendPasswordResetEmail } from 'firebase/auth';
 import { getAuth } from 'firebase/auth';
 import { app } from './firebase.js';
 
 export const auth = getAuth(app);
 export const singup = (email, password) => createUserWithEmailAndPassword(auth, email, password);
 export const signIn = (email, password) => signInWithEmailAndPassword(auth, email, password);
+export const resetPassword = (email) => sendPasswordResetEmail(auth, email);
 
 export const signInGoogle = () => {
   const provider = new GoogleAuthProvider();
