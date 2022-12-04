74fbe3046bbcfe0fcda01cb805a6eb7e3f903ef9
Erika Duarte <erika.duarte.mora@gmail.com>
2022-12-03 19:09:11 -0500

    test de función signUp

--- test/autentication.spec.js
+++ test/autentication.spec.js
@@ -7,7 +7,21 @@ describe('singup', () => {
   it('debería ser una función', () => {
     expect(typeof singup).toBe('function');
   });
-  it('deberia retornar un objeto con la propiedad email', () => singup('gaba@gmail.com', '1234567').then((user) => {
-    expect(user.email).toBe('gaba@gmail.com');
-  }));
+
+  it('Debe llamarse al método crear usuario', () => {
+    createUserWithEmailAndPassword.mockImplementation(() => Promise.resolve('gaba@gmail.com'));
+    singup(createUserWithEmailAndPassword);
+
+    expect(createUserWithEmailAndPassword).toBeCalled();
+  });
+
+  it('deberia retornar un objeto con la propiedad email', () => {
+    singup('gaba@gmail.com', '1234567').then((user) => {
+      expect(user).toBe('gaba@gmail.com');
+    });
+  });
+  it('Debe recibir parámetros', () => {
+    singup('gaba@gmail.com', '1234567');
+    expect('firebase/auth'.createUserWithEmailAndPassword).toHaveBeenCalledWith(3);
+  });
 });
